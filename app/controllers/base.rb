0374e90cd424e723c4ccf1901b2f2a795e446de8
Juan Camilo Bustamante <juancabuhe@gmail.com>
2017-02-14 23:12:37 -0500

    Vista de Sede V0.1

--- app/controllers/base.rb
+++ app/controllers/base.rb
@@ -14,4 +14,11 @@ Aquiub::App.controllers :base,  map: '/' do
     render :catalogo
   end
 
+  get :sede, with: :id do
+    @sede = Place.find(params[:id])
+    @service_pestanas = Service.where(place: @sede, type: 1)
+    @service_cejas = Service.where(place: @sede, type: 2)
+    render :sede
+  end
+
 end
--- models/place.rb
+++ models/place.rb
@@ -3,12 +3,37 @@ class Place
   include Mongoid::Document
 
   field :name,                          type: String
+  field :address,                       type: String
   field :latitude,                      type: String
   field :longitude,                     type: String
+
+  mount_uploader :header_picture,       ImageUploader
+  mount_uploader :pestanas_picture,     ImageUploader
+  mount_uploader :cejas_picture,        ImageUploader
 
   has_many :services
   has_many :place_pictures
 
   accepts_nested_attributes_for :place_pictures
+
+  def get_header_picture_url
+    self.header_picture_url.blank? ? '../img/foto-cabecera-completa.png' : self.header_picture_url
+  end
+
+  def get_pestanas_picture_url
+    self.pestanas_picture_url.blank? ? 'https://s3-us-west-2.amazonaws.com/laboratoday/public/images/default_profile_picture.png' : self.pestanas_picture_url
+  end
+  
+  def get_cejas_picture_url
+    self.cejas_picture_url.blank? ? 'https://s3-us-west-2.amazonaws.com/laboratoday/public/images/default_profile_picture.png' : self.cejas_picture_url
+  end
+
+  def get_place_pitures
+    urls = []
+    self.place_pictures.each do |p|
+      urls << p.get_picture_url
+    end
+    urls
+  end
 
 end
