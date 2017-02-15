6984f93d83a651e16334ed35158cbb1561224f2f
Santiago Jimenez <sago2k8@gmail.com>
2017-02-14 23:18:29 -0500

    home page instagram pictures

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -28,3 +28,44 @@
     }
   })
 })(jQuery) // End of use strict
+
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel({
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+    }
+  });
+
+$.ajax({
+   url: 'https://api.instagram.com/v1/users/self/media/recent/?access_token=590375326.1677ed0.b72ef6b8d08f437cba2a99c31bad7ee0',
+   data: {
+      format: 'json'
+   },
+   error: function() {
+      $('#gallery_container').html('<p>An error has occurred</p>');
+   },
+   dataType: 'jsonp',
+   success: function(data) {
+      console.log(data.data[0])
+      $.each(data.data, function( index, value ) {
+        console.log(value)
+        $( "#gallery_container" ).append( "<div class='col-sm-4 col-xs-6 col-md-3 col-lg-3'><a class='thumbnail fancybox' rel='ligthbox' href="+ value.images.standard_resolution.url+ "><img class='img-responsive' alt='' src="+ value.images.standard_resolution.url+  " /> </a> </div>" );
+      });
+
+   },
+   type: 'GET'
+});
+
+$(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+
