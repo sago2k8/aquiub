e7fd1a2f9ddd89178acb843866caa748d2f8a4d2
Santiago Jimenez <sago2k8@gmail.com>
2017-02-22 14:18:10 -0500

    mina config

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,6 +54,9 @@ task :setup => :environment do
   queue! %[mkdir -p "#{deploy_to}/#{shared_path}/sockets"]
   queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/sockets"]
 
+  queue! %[mkdir -p "#{deploy_to}/#{shared_path}/public/public"]
+  queue! %[chmod g+rx,u+rwx "#{deploy_to}/#{shared_path}/public/public"]
+
   # queue! %[touch "#{deploy_to}/#{shared_path}/config/database.yml"]
   queue! %[touch "#{deploy_to}/#{shared_path}/config/application.yml"]
   queue  %[echo "-----> Be sure to edit '#{deploy_to}/#{shared_path}/config/application.yml' and 'secrets.yml'."]
